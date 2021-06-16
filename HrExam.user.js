72616234bff11ba0bbabb60c6d28416cfbd39ebc
拉斯特 <last2003@yeah.net>
2021-06-16 14:39:48 +0800

    update HrExam.user.js.

--- HrExam.user.js
+++ HrExam.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name CCS在线答题
 // @namespace Violentmonkey Scripts
+// @version      20210616
 // @match http://apptest.ccs.org.cn/ccsic/exam/examQuestion
 // @updateURL    https://gitee.com/last2003/userscript/raw/master/HrExam.user.js
 // @downloadURL  https://gitee.com/last2003/userscript/raw/master/HrExam.user.js
