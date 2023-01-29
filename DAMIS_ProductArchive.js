56fd3e2ac785de50b42e6ffbc2e84ba47de91cc2
chenjun@work <chenjun@work>
2023-01-29 14:45:33 +0800

    2023年1月18日16:46:26

--- DAMIS_ProductArchive.js
+++ DAMIS_ProductArchive.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name damis 产品案卷归档
 // @namespace Violentmonkey Scripts
-// @match        http://damis.ccs.org.cn/damis/business/ywgl/danganshuju/DangAnKuServlet.do*
+// @match        http://damis.ccs.org.cn/damis/business/ywgl/danganshuju/DangAnKuServlet.do?method=addFile*
 // @updateURL    https://git.ccswz.top:10000/pub/userscript/raw/master/DAMIS_ProductArchive.js
 // @downloadURL  https://git.ccswz.top:10000/pub/userscript/raw/master/DAMIS_ProductArchive.js
 // @require      https://git.ccswz.top:10000/pub/userscript/raw/master/moment.min.js
@@ -12,18 +12,55 @@
 
 (function () {
     //code here
+    // 获取参数
+    var params = new URLSearchParams(window.location.search);
+    var physicsName = params.get("physicsName");
+    // 设置责任者
+    var zrz='申其兵';
 
-    $('input[value="<<<"]').click(function () {
-        fillin();
-    })
-    function fillin() {
-        $('select[name="BGQX"]').val('长期（30年）')
-        $('select[name="WJXT"]').val('电子')
-        // 设置责任者
-        $('input[name="ZRZ"]').val("申其兵")
+// 根据参数来执行函数
+    if (physicsName === "T_53D532CDF8E01") {
+        func1();
+    } else if (physicsName === "T_6223FDC875E68") {
+        func2();
+    } else {
+        console.log("Invalid physicsName value");
+    }
 
-        var today = moment();
-        $('input[name="BZRQ"]').val(today.format('YYYY-MM-DD'));
+    function func1(){
+        $('input[value="<<<"]').click(function () {
+            fillin();
+        })
+        function fillin() {
+            $('select[name="BGQX"]').val('长期（30年）')
+            $('select[name="WJXT"]').val('电子')
+
+            $('input[name="ZRZ"]').val(zrz);
+
+            var today = moment();
+            $('input[name="BZRQ"]').val(today.format('YYYY-MM-DD'));
+
+        }
+
+    }
+
+    function func2(){
+        const em='<input type="button" class="button" id="us_tc" value="填充" style="margin-right: 10px;" > ';
+        $('input[value="确定"]').before(em)
+
+        $('#us_tc').click(function () {
+            fillin();
+        })
+        function fillin() {
+            $('select[name="BGQX"]').val('长期（30年）')
+            $('select[name="MJ"]').val('内控B');
+            // 设置责任者
+            $('input[name="ZRZ"]').val(zrz);
+
+            // var today = moment();
+            // $('input[name="BZRQ"]').val(today.format('YYYY-MM-DD'));
+
+        }
 
     }
 
--- DAMIS_ProductArchiveList.js
+++ DAMIS_ProductArchiveList.js
@@ -23,34 +23,40 @@
     })
 
     function fillin() {
-        var usertableid = document.getElementsByName("usertableid")[0].value;
-        var physicsName = document.getElementsByName("physicsName")[0].value;
-        var gdOrdgd = document.getElementsByName("gdOrdgd")[0].value;
+        // var usertableid = document.getElementsByName("usertableid")[0].value;
+        // var physicsName = document.getElementsByName("physicsName")[0].value;
+        // var gdOrdgd = document.getElementsByName("gdOrdgd")[0].value;
         mygrid.getAllRowIds().split(',').forEach(function (rowid) {
-            let url = "./DangAnKuServlet.do?method=addFile&usertableid=" + usertableid + "&gdOrdgd=" + gdOrdgd + "&physicsName=" + physicsName + "&fileid=" + rowid + "&filemode=modify&showPosition=yes";
-            post_data(url);
+            // let url = "./DangAnKuServlet.do?method=addFile&usertableid=" + usertableid + "&gdOrdgd=" + gdOrdgd + "&physicsName=" + physicsName + "&fileid=" + rowid + "&filemode=modify&showPosition=yes";
+            // post_data(url);
+            setTimeout(() => {
+                mygrid.setSelectedRow(rowid);
+                modifyFile('yes');
+            }, 1000);
+
+
         });
     }
 
 
 
-    function post_data(url) {
-
-        $.get(url, function (page, status) {
-            const html = $(page);
-            const post_url = 'http://damis.ccs.org.cn/damis/business/ywgl/danganshuju/DangAnKuServlet.do?method=updateFile&pageNum=1&pageSize=30';
-            let theForm = $(html.find('form[name="addFileForm"]')[0]);
-            theForm.find('select[name="BGQX"]').val('长期（30年）');
-            theForm.find('select[name="MJ"]').val('内控B');
-            theForm.find('input[name="ZRZ"]').val("申其兵");
-            // theForm.find('input[name="CWRQ"]').val(moment().format('YYYY-MM-DD'));
-            console.log(theForm.serialize());
-
-            $.post(post_url, theForm.serialize(), function (data, status) {
-                console.log('------------------------------------');
-                console.log(url);
-            });
-        });
-    }
+    // function post_data(url) {
+    //
+    //     $.get(url, function (page, status) {
+    //         const html = $(page);
+    //         const post_url = 'http://damis.ccs.org.cn/damis/business/ywgl/danganshuju/DangAnKuServlet.do?method=updateFile&pageNum=1&pageSize=30';
+    //         let theForm = $(html.find('form[name="addFileForm"]')[0]);
+    //         theForm.find('select[name="BGQX"]').val('长期（30年）');
+    //         theForm.find('select[name="MJ"]').val('内控B');
+    //         theForm.find('input[name="ZRZ"]').val("申其兵");
+    //         // theForm.find('input[name="CWRQ"]').val(moment().format('YYYY-MM-DD'));
+    //         console.log(theForm.serialize());
+    //
+    //         $.post(post_url, theForm.serialize(), function (data, status) {
+    //             console.log('------------------------------------');
+    //             console.log(url);
+    //         });
+    //     });
+    // }
 
 })();
